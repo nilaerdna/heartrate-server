a458d1673b3b0b709a92baf57ee1eacd60a11e1c
Andrea <andrea.lin3110@gmail.com>
2021-01-23 14:06:40 +0100

    Finished /send Route and Upload to Database

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 const app = express();
-
 const port = process.env.PORT || 3000;
-
-// create application/json parser
-var jsonParser = bodyParser.json();
-
-// create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+dotenv.config();
 
-app.get('/', (req, res) => {
-    return res.send('Received a GET HTTP method');
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connection
+    .once('open', () => {
+        console.log('Connected to the server');
+
+        const HeartRateSchema = mongoose.Schema({
+            heartRate: Number,
+            timeStamp: Number,
+        });
+
+        const HeartRate = mongoose.model('HeartRate', HeartRateSchema, 'heartRates');
+
+        app.post('/send', urlencodedParser, async (req, res) => {
+            const { hr } = JSON.parse(JSON.stringify(req.body));
+            console.log(hr);
+
+            const hrObj = {
+                heartRate: hr,
+                timeStamp: Date.now(),
+            };
+
+            HeartRate.collection.insertOne(hrObj, (err, docs) => {
+                if (err) {
+                    return console.error(error);
+                } else {
+                    console.log('A document was inserted in the database.');
+                }
+            });
+
+            return res.send('Received a POST HTTP method');
+        });
+    })
+    .on('error', (error) => {
+        console.log(error);
+    });
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
 });
-
-
-app.post('/send', urlencodedParser, (req, res) => {
-    const { hr } = JSON.parse(JSON.stringify(req.body));
-    console.log(hr);
-    return res.send('Received a POST HTTP method');
-});
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
