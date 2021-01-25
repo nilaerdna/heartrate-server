04404c63a2cfe4c8c512c29cdda6d9877996d85d
Andrea <andrea.lin3110@gmail.com>
2021-01-25 21:12:51 +0100

    CORS

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
