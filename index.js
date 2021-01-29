9967ef1a0545edce5e8835b636918ee24095e967
Andrea <andrea.lin3110@gmail.com>
2021-01-29 22:38:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ mongoose.connection
                 .then(() => {
                     const responseJson = {
                         title: `Last ${numberOfDocs} heart rates`,
-                        date: valueArray,
+                        data: valueArray,
                     };
 
                     return res.send(JSON.stringify(responseJson));
